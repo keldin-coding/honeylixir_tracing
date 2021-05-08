a3c909aad1f71fe1274d9fb12eeebe5e1bc80003
Jon Anderson <8674577+lirossarvet@users.noreply.github.com>
2021-05-08 11:58:06 -0700

    Release 0.3.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule HoneylixirTracing.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/lirossarvet/honeylixir_tracing"
-  @version "0.3.0"
+  @version "0.3.1"
 
   def project do
     [
